9761f6cf5657783147c3a9061a782364bbf87508
notwonderful <ratatatata322@gmail.com>
2022-11-20 08:38:19 +0300

    Refactoring the account creation logic

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
+use App\Services\SRP6Service;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -31,22 +32,23 @@ class RegisteredUserController extends Controller
      *
      * @throws \Illuminate\Validation\ValidationException
      */
-    public function store(Request $request)
+    public function store(Request $request, SRP6Service $SRP6Service)
     {
-        $request->validate([
+        $validated = $request->validate([
             'username' => ['required', 'string', 'max:20', 'min:3', 'unique:account'],
             'email' => ['required', 'string', 'email:rfc,dns', 'max:255', 'unique:account'],
             'password' => ['required', 'confirmed', Rules\Password::min(4)],
             'g-recaptcha-response' => 'required|captcha'
         ]);
+        $validated['username'] = strtoupper($validated['username']);
 
-        list($salt, $verifier) = $this->getRegistrationData(strtoupper($request->username), $request->password);
+        $verify = $SRP6Service->getSaltAndVerifier($validated['username'], $validated['password']);
 
         $user = User::create([
-            'username' => $request->username,
-            'email' => $request->email,
-            'salt' => $salt,
-            'verifier' => $verifier,
+            'username' => $validated['username'],
+            'email' => $validated['email'],
+            'salt' => $verify['salt'],
+            'verifier' => $verify['verifier'],
             'expansion' => 2
         ]);
 
